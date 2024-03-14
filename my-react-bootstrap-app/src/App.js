eb72fd8a4db7c724d6d8bd182d552ac72ed30742
PeasantL <samuel.tan99@rocketmail.com>
2024-03-15 01:41:26 +0800

    Added data loading from json file.

--- my-react-bootstrap-app/src/App.js
+++ my-react-bootstrap-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Image} from 'react-bootstrap';
 import { TextForm } from './components/form';
 
@@ -26,6 +26,27 @@ export default function App () {
     });
   };
 
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/data.json')
+      .then(response => response.json())
+      .then(jsonObject => {
+        setData(jsonObject)
+        setTextValues(prevValues => ({
+          ...prevValues,
+          ...Object.keys(prevValues).reduce((acc, key) => {
+            if (jsonObject.data.hasOwnProperty(key)) {
+              acc[key] = jsonObject.data[key];
+            }
+            return acc;
+        }, {})
+      }));
+    })
+    .catch(error => console.error('There was a problem with the fetch operation', error));
+  }, []);
+
+
   return (
     <div class= "mt-3 mb-3">
       <Container> 
@@ -33,6 +54,9 @@ export default function App () {
           <Image src="main_awkward-questions-fm_spec_v2.png" fluid alt="Image" className="img-fluid mx-auto d-block" rounded/>
         </div>
           <TextForm textValues={textValues} handleChange={handleChange} />
+      </Container>
+      <Container>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
       </Container>
     </div>
   );
