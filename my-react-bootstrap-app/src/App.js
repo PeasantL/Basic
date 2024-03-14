dea6bd9186b53fe7bfa3df1057b77d1f7b0241d2
PeasantL <samuel.tan99@rocketmail.com>
2024-03-15 03:22:32 +0800

    Linked changes between textValue states and Json States

--- my-react-bootstrap-app/src/App.js
+++ my-react-bootstrap-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Image} from 'react-bootstrap';
+import { Container, Image, Row, Col, Button} from 'react-bootstrap';
 import { TextForm } from './components/form';
 
 
@@ -15,27 +15,43 @@ export default function App () {
     creator_notes: "",
     system_prompt: "",
     post_history_instructions: "",
-    creator: ""
+    creator: "" 
   });
 
+
+  //Todo: change every text box to be disabled until there is a file loaded!  
+  //Handle Change should be invoke if there is a loaded json, so that the 
+  //JSON data is changed with changes in text
   const handleChange = (event) => {
     const { name, value } = event.target; // Destructure name and value from event.target
     setTextValues({
       ...textValues,
       [name]: value // Use computed property name to update the right part of the state
     });
+
+    //
+    setData(prevData => ({
+      ...prevData,
+      data: { ...prevData.data, [name]: value}
+    }));
+
   };
 
-  const [data, setData] = useState(null);
+  const [jsonObject, setData] = useState(null);
 
+  //to be changed to recieve from API 
+  //Update textValues upon loading the fetched data
   useEffect(() => {
     fetch('/data.json')
       .then(response => response.json())
       .then(jsonObject => {
-        setData(jsonObject)
+        setData(jsonObject); //Sets the fetched data to state
+
+        // Updates textValues based on the fetched data
         setTextValues(prevValues => ({
           ...prevValues,
           ...Object.keys(prevValues).reduce((acc, key) => {
+            // Check if the fetched data has the key and if so, update the value 
             if (jsonObject.data.hasOwnProperty(key)) {
               acc[key] = jsonObject.data[key];
             }
@@ -45,18 +61,33 @@ export default function App () {
     })
     .catch(error => console.error('There was a problem with the fetch operation', error));
   }, []);
+  
 
+  /* App Structure */
 
   return (
     <div class= "mt-3 mb-3">
       <Container> 
         <div class= "mb-5">
-          <Image src="main_awkward-questions-fm_spec_v2.png" fluid alt="Image" className="img-fluid mx-auto d-block" rounded/>
+          <Row>
+            <Col>
+              <div className="d-grid gap-4">
+              <Button variant="primary" size="lg">Load File</Button>
+              <Button variant="primary" size="lg">Save File</Button>
+              <Button variant="primary" size="lg">Reset File</Button>
+              </div>
+            </Col>
+            <Col>
+              <Image src="main_awkward-questions-fm_spec_v2.png" fluid alt="Image" className="img-fluid mx-auto d-block" rounded/>
+            </Col>
+          </Row>
         </div>
+        {/*Text boxes rendering*/}
           <TextForm textValues={textValues} handleChange={handleChange} />
       </Container>
+
       <Container>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <pre>{JSON.stringify(jsonObject, null, 2)}</pre>
       </Container>
     </div>
   );
--- my-react-bootstrap-app/src/components/form.js
+++ my-react-bootstrap-app/src/components/form.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import { Form } from 'react-bootstrap'
 
-export function CheckAndRadio() {
-  return (
-    <Form>
-      {['checkbox', 'radio'].map((type) => (
-        <div key={`default-${type}`} className="mb-3">
-          <Form.Check // prettier-ignore
-            type={type}
-            id={`default-${type}`}
-            label={`default ${type}`}
-          />
-
-          <Form.Check
-            disabled
-            type={type}
-            label={`disabled ${type}`}
-            id={`disabled-default-${type}`}
-          />
-        </div>
-      ))}
-    </Form>
-  );
-}
 
 export const TextForm = ({ textValues, handleChange}) => {
   return (
