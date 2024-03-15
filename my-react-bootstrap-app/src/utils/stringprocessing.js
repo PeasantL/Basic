fc4fc0132eafb5068817bce29067f4ab079f2593
PeasantL <samuel.tan99@rocketmail.com>
2024-03-16 07:56:07 +0800

    Moved file, string processing fully working

--- my-react-bootstrap-app/src/utils/stringprocessing.js
+++ my-react-bootstrap-app/src/utils/stringprocessing.js
@@ -1,6 +1,4 @@
-const testString = "part1 *test* someime *adg*\n\n\n*part2 agfsarfgs*part3 part4 *yes* part 6\n\n\npart4*dass*dsfgsdgdgfd*f*";
-
-function processStringBasedOnCharacters(inputString) {
+export function processString(inputString) {
   // Check for the presence of both asterisks and quotes
   if (inputString.includes('*') && inputString.includes('"')) {
       // Do nothing, string is correct
@@ -11,7 +9,7 @@ function processStringBasedOnCharacters(inputString) {
 
   // Check for the presence of only asterisks
   else if (inputString.includes('*')) {
-      // Split the input string by '\n', capturing the newlines in the result
+    // Split the input string by '\n', capturing the newlines in the result
     let finalArray = inputString.split(/(\n+)/);
 
     // Process each element before splitting into sub-arrays
@@ -63,7 +61,57 @@ function processStringBasedOnCharacters(inputString) {
   // Check for the presence of only quotes
   else if (inputString.includes('"')) {
       // Apply processing for quotes only
-      return handleQuotes(inputString);
+
+      // Split the input string by '\n', capturing the newlines in the result
+      let finalArray = inputString.split(/(\n+)/);
+
+      // Process each element before splitting into sub-arrays
+      finalArray = finalArray.map(element => {
+          // Directly return if the element is purely newline(s)
+          if (/^(\n+)$/.test(element)) {
+              return element;
+          }
+
+          // Process if the element contains text
+          if (element[0] !== '\"') {
+              // Element doescle not start with an asterisk
+              let parts = element.split('\"').filter(part => part !== '').map(part => part.trim());
+
+              return parts.map((part, index) => {
+                  if (index % 2 === 0) { // Odd element (even index), wrap with quotes
+                      return '*' + part + '*';
+                  } else { // Even element (odd index), wrap with asterisks
+                      return '\\"' + part + '\\"';
+                  }
+              }).join(' ');
+          } else {
+              // Element starts with an asterisk
+              let parts = element.split('\"').filter(part => part !== '').map(part => part.trim());
+              return parts.map((part, index) => {
+                  if (index % 2 === 0) { // Odd element (even index), wrap with asterisks
+                      return '\\"' + part + '\\"';
+                  } else { // Even element (odd index), wrap with quotes
+                      return '*' + part + '*';
+                  }
+              }).join(' ');
+          }
+      });
+
+      // Convert the array back into a string, handling spacing around newlines
+      let finalString = finalArray.reduce((acc, element, index, array) => {
+          if (/^(\n+)$/.test(element)) {
+              // Element is newline(s), add without space
+              return acc + element;
+          } else {
+              // Element is text, add with space unless the next element is a newline
+              let nextElement = array[index + 1];
+              let addSpace = nextElement && /^(\n+)$/.test(nextElement) ? '' : ' ';
+              return acc + element + addSpace;
+          }
+      }, '').trim(); // Trim the final string to remove any leading/trailing spaces
+      return finalString
+
+
   } 
   // If none of the above, return the string unmodified or apply other logic
   else {
@@ -72,4 +120,10 @@ function processStringBasedOnCharacters(inputString) {
   }
 }
 
-console.log(processStringBasedOnCharacters(testString))
+/*
+const testString = "part1 *test* someime *adg*\n\n\n*part2 agfsarfgs*part3 part4 *yes* part 6\n\n\npart4*dass*dsfgsdgdgfd*f*";
+const quoteString = 'part1 \"test\" someime \"adg\"\n\n\n\"part2 agfsarfgs\"part3 part4 \"yes\" part 6\n\n\npart4\"dass\"dsfgsdgdgfd\"f\"'
+
+console.log(processString(testString))
+console.log(processString(quoteString))
+*/
